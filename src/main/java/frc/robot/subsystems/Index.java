0f8ad57d3c461fdebca9f81411176a5e48469a79
ATurner8133 <156738528+ATurner8133@users.noreply.github.com>
2024-01-25 01:45:44 -0500

    Update Index.java

--- src/main/java/frc/robot/subsystems/Index.java
+++ src/main/java/frc/robot/subsystems/Index.java
@@ -3,14 +3,17 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkLowLevel;
 import com.revrobotics.CANSparkMax;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Index extends SubsystemBase{
     public CANSparkMax leftSide;
     public CANSparkMax rightSide;
+    public DigitalInput storeNote;
     public Index() {
         leftSide = new CANSparkMax(Constants.Indexer.leftSide, CANSparkLowLevel.MotorType.kBrushless);
         rightSide = new CANSparkMax(Constants.Indexer.rightSide, CANSparkLowLevel.MotorType.kBrushless);
+        storeNote = new DigitalInput(Constants.Indexer.storeNote);
     }
 }
