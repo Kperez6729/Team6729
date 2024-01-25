51aac0ce57223b23390a598cd4421659158fd0fa
ATurner8133 <156738528+ATurner8133@users.noreply.github.com>
2024-01-25 01:51:08 -0500

    Create Shooter.java

---
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,15 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Shooter extends SubsystemBase {
+    public TalonFX leftWheel;
+    public TalonFX rightWheel;
+    public Shooter() {
+        leftWheel = new TalonFX(Constants.Shooter.leftWheel);
+        rightWheel = new TalonFX(Constants.Shooter.rightWheel);
+    }
+}
