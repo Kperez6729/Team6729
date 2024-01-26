5e45b8383ffe12d0ad11796fb1ca888a082bb860
ATurner8133 <156738528+ATurner8133@users.noreply.github.com>
2024-01-26 01:28:30 -0500

    Create runClimber.java

---
+++ src/main/java/frc/robot/commands/runClimber.java
@@ -0,0 +1,33 @@
+package frc.robot.commands;
+
+import java.util.function.BooleanSupplier;
+
+import com.ctre.phoenix6.controls.DutyCycleOut;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.subsystems.Climber;
+
+public class runClimber extends Command{
+    private BooleanSupplier a;
+    private BooleanSupplier b;
+    public runClimber(BooleanSupplier a, BooleanSupplier b){
+        this.a = a;
+        this.b = b;
+    }
+    @Override
+    public void execute(){
+        if (a.getAsBoolean()) {
+            Climber.leftArm.setControl(new DutyCycleOut(-Constants.Climber.speed));
+            Climber.rightArm.setControl(new DutyCycleOut(Constants.Climber.speed));
+        }
+        else if(b.getAsBoolean()){
+            Climber.rightArm.setControl(new DutyCycleOut(-Constants.Climber.speed));
+            Climber.leftArm.setControl(new DutyCycleOut(Constants.Climber.speed));
+        }
+        else{
+            Climber.rightArm.setControl(new DutyCycleOut (0));
+            Climber.leftArm.setControl(new DutyCycleOut(0));
+        }
+    }
+}
