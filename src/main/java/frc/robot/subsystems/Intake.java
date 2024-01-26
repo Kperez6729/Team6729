3bb1c999bdcc97a73431834a39931106222d2a3d
ATurner8133 <156738528+ATurner8133@users.noreply.github.com>
2024-01-25 22:41:16 -0500

    Update Intake.java

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -6,8 +6,8 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Intake extends SubsystemBase{
-   public TalonSRX topRoller;
-   public TalonSRX bottomRoller;
+   public static TalonSRX topRoller;
+   public static TalonSRX bottomRoller;
     public Intake(){
         topRoller = new TalonSRX(Constants.Intake.topRoller);
         bottomRoller = new TalonSRX(Constants.Intake.bottomRoller);
