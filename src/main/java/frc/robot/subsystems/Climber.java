89cd8897dcfd5d7f492dee6ed7f927d5fd4a8815
ATurner8133 <156738528+ATurner8133@users.noreply.github.com>
2024-01-25 22:41:11 -0500

    Update Climber.java

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -2,18 +2,18 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.TalonFX;
 
-import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Climber extends SubsystemBase{
     
-    public TalonFX leftArm;
-    public TalonFX rightArm;
-    public DutyCycleEncoder armPosition;
+    public static TalonFX leftArm;
+    public static TalonFX rightArm;
+    public static Encoder armPosition;
     public Climber() {
         leftArm = new TalonFX(Constants.Climber.leftArm);
         rightArm = new TalonFX(Constants.Climber.rightArm);
-        armPosition = new DutyCycleEncoder(Constants.Climber.armPosition);
+        armPosition = new Encoder(Constants.Climber.armPosition1, Constants.Climber.armPosition2);
     }
 }
