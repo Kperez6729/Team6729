09337659cd7b86070921bc8d5c576c876d19cedf
ATurner8133 <156738528+ATurner8133@users.noreply.github.com>
2024-04-08 10:44:44 -0400

    Git hub timestamps and version numbers added

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "Team6729";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 91;
+  public static final String GIT_SHA = "66e7aef7e0ab815fd9b0af6c6c1ae6259d8eddcb";
+  public static final String GIT_DATE = "2024-04-05 20:02:30 EDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2024-04-08 10:42:22 EDT";
+  public static final long BUILD_UNIX_TIME = 1712587342581L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
