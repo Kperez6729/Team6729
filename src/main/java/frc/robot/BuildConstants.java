99fb21d93c12ebb729f15106d05e7567290f4f71
ElxMatador <k.perez865@gmail.com>
2024-04-09 00:28:01 -0400

    Fixed build error

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Team6729";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 92;
-  public static final String GIT_SHA = "09337659cd7b86070921bc8d5c576c876d19cedf";
-  public static final String GIT_DATE = "2024-04-08 10:44:44 EDT";
+  public static final int GIT_REVISION = 93;
+  public static final String GIT_SHA = "ab8e3c89a6521426cc3a639f09df390766d72d35";
+  public static final String GIT_DATE = "2024-04-08 11:11:53 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-04-08 11:09:22 EDT";
-  public static final long BUILD_UNIX_TIME = 1712588962760L;
+  public static final String BUILD_DATE = "2024-04-09 00:22:51 EDT";
+  public static final long BUILD_UNIX_TIME = 1712636571696L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
